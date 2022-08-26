fe576ed714e763410e867a0a26087b274e264c00
nour-aldin <nourdx73@gmail.com>
2022-08-26 21:31:53 +0200

    Hero Component finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,15 +18,33 @@ const Hero = () => (
     <div className='flex flex-row justify-between items-center w-full '>
       <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>The Next <br className='sm:block hidden'/> {' '}
         <span className='text-gradient'>Generation</span> {' '}
-        Payment Method. 
       </h1>
       <div className='ss:flex hidden md:mr-4 mr-0 '>
         <GetStarted/>
       </div>
-        
     </div>
+    <h1 className='font-poppins font-semibold 
+    ss:text-[68px] text-[52px] text-white 
+    ss:leading-[100px] leading-[75px] w-full'>
+      Payment Method.
+    </h1>
+    <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+    With the right credit card, you can improve your financial life by 
+    building credit, earning rewards and saving money. But with 
+    hundreds of credit cards on the market.
+    </p>
   </div>
-
+  <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+    <img src={robot} alt='robot'
+      className='w-[100%] h-[100%] relative z-[5]'
+    />
+    <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
+    <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient'/>
+    <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'/>
+  </div>
+  <div className={`ss:hidden ${styles.flexCenter} `}>
+    <GetStarted/>
+  </div>
   </section>
 )
 export default Hero
